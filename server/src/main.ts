fbcfd079b75d553ac10091373eed891f47058bdd
nikolayhotim1 <hotim_nikolay@mail.ru>
2022-02-06 20:19:51 +0300

    first commit

---
+++ server/src/main.ts
@@ -0,0 +1,14 @@
+import { AppModule } from './app.module';
+import { NestFactory } from '@nestjs/core';
+
+const start = async () => {
+    try {
+        const PORT = process.env.PORT || 5000;
+        const app = await NestFactory.create(AppModule, { bodyParser: false });
+        await app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
+    } catch (e) {
+        console.log(e);
+    }
+};
+
+start();
